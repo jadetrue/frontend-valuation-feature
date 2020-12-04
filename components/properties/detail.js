b37637421a73f8866bf8c56734b90c7563441cc4
Jack Scott <jackscott@MFT-A068.local>
2020-12-04 17:35:44 +0000

    Use account endpoint with useEffect

--- components/properties/detail.js
+++ components/properties/detail.js
@@ -1,8 +1,8 @@
 /* eslint-disable max-statements */
-import PropTypes from "prop-types"
-import React, {useEffect} from "react"
-import {add, format} from "date-fns"
-import {Button} from "../button"
+import PropTypes from "prop-types";
+import React, { useEffect } from "react";
+import { add, format } from "date-fns";
+import { Button } from "../button";
 
 import {
   AccountHeadline,
@@ -12,15 +12,15 @@ import {
   InfoText,
   Inset,
   RowContainer,
-} from "./style"
+} from "./style";
 
 const Detail = ({}) => {
   useEffect(() => {
     window
-      .fetch("/api/hello")
+      .fetch("/api/account")
       .then((res) => res.json())
-      .then(console.log)
-  }, [])
+      .then(console.log);
+  }, []);
 
   const account = {
     uid: "65156cdc-5cfd-4b34-b626-49c83569f35e",
@@ -33,7 +33,7 @@ const Detail = ({}) => {
     subType: "residential",
     originalPurchasePrice: 250000,
     originalPurchasePriceDate: "2017-12-03",
-    recentValuation: {amount: 310000, status: "good"},
+    recentValuation: { amount: 310000, status: "good" },
     associatedMortgages: [
       {
         name: "HSBC Repayment Mortgage",
@@ -45,12 +45,12 @@ const Detail = ({}) => {
     postcode: "BS1 2AA",
     lastUpdate: "2020-12-01T08:55:33.421Z",
     updateAfterDays: 30,
-  }
+  };
 
-  let mortgage
-  const lastUpdate = new Date(account.lastUpdate)
+  let mortgage;
+  const lastUpdate = new Date(account.lastUpdate);
   if (account.associatedMortgages.length) {
-    mortgage = account.associatedMortgages[0]
+    mortgage = account.associatedMortgages[0];
   }
 
   return (
@@ -66,7 +66,7 @@ const Detail = ({}) => {
           </InfoText>
           <InfoText>
             {`Next update ${format(
-              add(lastUpdate, {days: account.updateAfterDays}),
+              add(lastUpdate, { days: account.updateAfterDays }),
               "do MMM yyyy"
             )}`}
           </InfoText>
@@ -99,7 +99,7 @@ const Detail = ({}) => {
       )}
       <Button>Edit account</Button>
     </Inset>
-  )
-}
+  );
+};
 
-export default Detail
+export default Detail;
---
+++ pages/api/account.js
@@ -0,0 +1,29 @@
+export default (req, res) => {
+  res.statusCode = 200;
+  res.json({
+    account: {
+      uid: "65156cdc-5cfd-4b34-b626-49c83569f35e",
+      deleted: false,
+      dateCreated: "2020-12-03T08:55:33.421Z",
+      currency: "GBP",
+      name: "15 Temple Way",
+      bankName: "Residential",
+      type: "properties",
+      subType: "residential",
+      originalPurchasePrice: 250000,
+      originalPurchasePriceDate: "2017-12-03",
+      recentValuation: '{amount: 310000, status: "good"}',
+      associatedMortgages: [
+        {
+          name: "HSBC Repayment Mortgage",
+          uid: "fb463121-b51a-490d-9f19-d2ea76f05e25",
+          currentBalance: -175000,
+        },
+      ],
+      canBeManaged: false,
+      postcode: "BS1 2AA",
+      lastUpdate: "2020-12-03T08:55:33.421Z",
+      updateAfterDays: 30,
+    },
+  });
+};
